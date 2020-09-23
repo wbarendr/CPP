dcdf553ac6a67371c17b9f9e3fc4cf022bbe922c
Wester Barendregt <wester@Westers-MacBook-Pro.local>
2020-09-23 14:29:28 +0200

    module 6 ex00 finished

---
+++ 6_module/ex00/convert_string.cpp
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   convert_string.cpp                                 :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: Wester <Wester@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/09/11 11:27:34 by Wester        #+#    #+#                 */
+/*   Updated: 2020/09/23 14:27:46 by wester        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <stdlib.h>
+#include <iomanip>
+
+bool        find_dot(std::string str){
+	for (int i = 0; str[i]; ++i){
+		if (str[i] == '.')
+			return true;
+	}
+	if (str == "+inf" || str == "-inf" || str == "nan")
+		return true;
+	return false;
+}
+
+bool        find_num(std::string str){
+	char nums[11] = "0123456789";
+
+	for (int i = 0; nums[i]; ++i){
+		if (str[0] == nums[i] || str[1] == nums[i])
+			return true;
+	}
+	return false;
+}
+
+void        print_char(int num){
+	if (isprint(num))
+		std::cout << "char " << static_cast<char>(num) << std::endl;
+	else
+		std::cout << "char: Non displayable" << std::endl;
+}
+
+void        convert_float(std::string str){
+	if (str == "+inff" || str == "-inff" || str == "nanf"){
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: " << str << std::endl;
+		str.back() = 0;
+		std::cout << "double: " << str << std::endl;
+	}
+	float fl = std::stof(str);
+		
+	print_char(static_cast<int>(fl));
+	std::cout << "int: " << static_cast<int>(fl) << std::endl;
+	std::cout << "float: " << str << std::endl;
+	str.back() = 0;
+	std::cout << "double: " << str << std::endl;
+	
+}
+
+void        convert_double(std::string str){
+	if (str == "+inf" || str == "-inf" || str == "nan"){
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: " << str << "f" << std::endl;
+		std::cout << "double: " << str << std::endl;
+		return ;
+	}
+	
+	long double db = std::stof(str);
+	
+	print_char(static_cast<int>(db));
+	std::cout << "int: " << static_cast<int>(db) << std::endl;
+	std::cout << "float: " << std::setprecision(str.length()) << str << "f" << std::endl;
+	std::cout << "double: " << str << std::endl;
+	
+}
+
+void        convert_int(std::string str){
+	int num = std::stoi(str);
+	
+	print_char(num);
+	std::cout << "int: " << num << std::endl;
+	std::cout << "float: " << num << ".0f" << std::endl;
+	std::cout << "double: " << num << ".0" << std::endl;
+}
+
+void		convert_char(std::string str){
+	int num = static_cast<int>(str[0]);
+	
+	print_char(num);
+	std::cout << "int: " << num << std::endl;
+	std::cout << "float: " << num << ".0f" << std::endl;
+	std::cout << "double: " << num << ".0" << std::endl;
+		
+}
+
+void        find_type(std::string str){
+	if (str.back() == 'f' && str[1] != 0 && str != "+inf" && str != "-inf")
+		convert_float(str);
+	else if (find_dot(str))
+		convert_double(str);
+	else if (find_num(str))
+		convert_int(str);
+	else if (isprint(str[0]) && !str[1])
+		convert_char(str);
+	else
+		std::cout << "something went terribly wrong!" << std::endl;
+}
+
+int         main(int argc, char **argv){
+	if (argc != 2){
+		std::cout << "feed me one string please" << std::endl;
+		return 1;
+	}
+	find_type(argv[1]);
+	return 0;
+}
